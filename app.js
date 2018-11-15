57c980f421a87f9e2612c2c9596e6dea9399f30c
Dao Pham <daopm99@gmail.com>
2018-11-15 13:42:34 +0000

    Chore: add pages and layout

--- app.js
+++ app.js
@@ -6,27 +6,39 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
-var menuList = ["Black Coffee", "White Coffee", "Brown Coffee"];
-
-app.get("/api/greeting", (req, res) => {
-    const { name } = req.query;
-    res.json({message: `Hi ${name}`})
-})
+var menuList = [
+    { name: "Black coffee", image: "https://farm9.staticflickr.com/8452/7933826484_1f729289c8.jpg"},
+    { name: "White coffee", image: "https://farm6.staticflickr.com/5260/5482639993_37b0d56c3e.jpg"},
+    { name: "Brown coffee", image: "https://farm2.staticflickr.com/1919/45595636631_d872389edc.jpg"},
+    { name: "Black coffee", image: "https://farm9.staticflickr.com/8452/7933826484_1f729289c8.jpg"},
+    { name: "White coffee", image: "https://farm6.staticflickr.com/5260/5482639993_37b0d56c3e.jpg"},
+    { name: "Brown coffee", image: "https://farm2.staticflickr.com/1919/45595636631_d872389edc.jpg"},
+    { name: "Black coffee", image: "https://farm9.staticflickr.com/8452/7933826484_1f729289c8.jpg"},
+    { name: "White coffee", image: "https://farm6.staticflickr.com/5260/5482639993_37b0d56c3e.jpg"},
+    { name: "Brown coffee", image: "https://farm2.staticflickr.com/1919/45595636631_d872389edc.jpg"},
+];
 
 app.get("/", (req, res) => {
     // res.json({message: "Hello"})
     res.render("home");
 });
 
-app.post("/addmenu", (req, res) => {
-    var coffee = req.body.newcoffee;
-    menuList.push(coffee);
-//   res.send("Post request");
-    res.redirect("/menu"); 
-});
-
 app.get("/menu", (req, res) => {
    res.render("menu", {menu: menuList}); 
+});
+
+app.get("/menu/new", (req, res) => {
+   res.render("new"); 
+});
+
+app.post("/menu", (req, res) => {
+    var coffee = req.body.newcoffee;
+    var url = req.body.image;
+    var item = { name: coffee, image: url};
+    
+    menuList.push(item);
+//   res.send("Post request");
+    res.redirect("/menu"); 
 });
 
 app.listen(process.env.PORT, process.env.IP, () => {
