397713c8b3013f967eba297236006b8ae3fdccf9
Dao Pham <daopm99@gmail.com>
2018-11-16 04:41:59 +0000

    Chore: add database and stuff

--- app.js
+++ app.js
@@ -1,58 +1,45 @@
-const express = require("express");
+const express    = require("express");
 const bodyParser = require("body-parser");
+const mongoose   = require("mongoose");
+var app = express();
 
-const app = express();
-const port = process.env.PORT || 3000;
-
+mongoose.connect('mongodb://localhost:27017/coffee', { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 
-const menuList = [
-  {
-    name: "Black coffee",
-    image: "https://farm9.staticflickr.com/8452/7933826484_1f729289c8.jpg"
-  },
-  {
-    name: "White coffee",
-    image: "https://farm6.staticflickr.com/5260/5482639993_37b0d56c3e.jpg"
-  },
-  {
-    name: "Brown coffee",
-    image: "https://farm2.staticflickr.com/1919/45595636631_d872389edc.jpg"
-  },
-  {
-    name: "Black coffee",
-    image: "https://farm9.staticflickr.com/8452/7933826484_1f729289c8.jpg"
-  },
-  {
-    name: "White coffee",
-    image: "https://farm6.staticflickr.com/5260/5482639993_37b0d56c3e.jpg"
-  },
-  {
-    name: "Brown coffee",
-    image: "https://farm2.staticflickr.com/1919/45595636631_d872389edc.jpg"
-  },
-  {
-    name: "Black coffee",
-    image: "https://farm9.staticflickr.com/8452/7933826484_1f729289c8.jpg"
-  },
-  {
-    name: "White coffee",
-    image: "https://farm6.staticflickr.com/5260/5482639993_37b0d56c3e.jpg"
-  },
-  {
-    name: "Brown coffee",
-    image: "https://farm2.staticflickr.com/1919/45595636631_d872389edc.jpg"
-  }
-];
+var menuSchema = new mongoose.Schema({
+  name: String,
+  image: String
+});
+
+var Menu = mongoose.model("Menu", menuSchema);
+
+// Menu.create({
+//     name: "Brown coffee",
+//     image: "https://farm2.staticflickr.com/1919/45595636631_d872389edc.jpg"
+// }, (err, coffee) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(coffee);
+//   }
+// });
+
 
 app.get("/", (req, res) => {
   res.render("home");
 });
 
 app.get("/menu", (req, res) => {
-  res.render("menu", { menu: menuList });
+  Menu.find({}, (err, menuList) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("menu", { menu: menuList });
+    }
+  });
+  
 });
 
 app.get("/menu/new", (req, res) => {
@@ -60,14 +47,20 @@ app.get("/menu/new", (req, res) => {
 });
 
 app.post("/menu", (req, res) => {
-  const coffee = req.body.newcoffee;
-  const url = req.body.image;
-  const item = { name: coffee, image: url };
+  var coffee = req.body.newcoffee;
+  var url = req.body.image;
+  var item = { name: coffee, image: url };
 
-  menuList.push(item);
-  res.redirect("/menu");
+  Menu.create(item, (err, item) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect("/menu");
+    }
+  });
+  
 });
 
-app.listen(port, process.env.IP, () => {
+app.listen(process.env.PORT, process.env.IP, () => {
   console.log("Server running");
 });
