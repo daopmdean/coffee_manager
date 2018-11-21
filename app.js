4f05b4b4799c5a35cc2d514d5ec876e97f937d0f
Dao Pham <daopm99@gmail.com>
2018-11-21 02:26:03 +0000

    add edit, index and show page

--- app.js
+++ app.js
@@ -2,22 +2,28 @@ const express    = require("express");
 const bodyParser = require("body-parser");
 const mongoose   = require("mongoose");
 var app = express();
+// mongodb://localhost:27017/coffee
+const MONGODB_URL = "mongodb+srv://daopham:test123@learning-ublv7.mongodb.net/CoffeeManager?retryWrites=true";
 
-mongoose.connect('mongodb://localhost:27017/coffee', { useNewUrlParser: true });
+mongoose.connect(MONGODB_URL, { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 
 var menuSchema = new mongoose.Schema({
   name: String,
-  image: String
+  image: String,
+  description: String,
+  price: Number
 });
 
 var Menu = mongoose.model("Menu", menuSchema);
 
 // Menu.create({
-//     name: "Brown coffee",
-//     image: "https://farm2.staticflickr.com/1919/45595636631_d872389edc.jpg"
+//     name: "Mocha",
+//     image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqPRzZ6B7cd5lFBB6fv0_i0VCVTFchVgF2yqSWziNo00Eg49Bs0Q",
+//     description: "Mix between Coffee and Chocolate",
+//     price: 3.99
 // }, (err, coffee) => {
 //   if (err) {
 //     console.log(err);
@@ -36,7 +42,7 @@ app.get("/menu", (req, res) => {
     if (err) {
       console.log(err);
     } else {
-      res.render("menu", { menu: menuList });
+      res.render("index", { menu: menuList });
     }
   });
   
@@ -44,6 +50,16 @@ app.get("/menu", (req, res) => {
 
 app.get("/menu/new", (req, res) => {
   res.render("new");
+});
+
+app.get("/menu/:id", (req, res) => {
+  Menu.findById(req.params.id, (err, foundItem) => {
+    if (err) {
+      res.redirect("/menu");
+    } else {
+      res.render("show", {item: foundItem});
+    }
+  });
 });
 
 app.post("/menu", (req, res) => {
